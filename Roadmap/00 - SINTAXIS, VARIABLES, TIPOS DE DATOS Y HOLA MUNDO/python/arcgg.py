458f5be001850b581ffcd71992a69409664ff481
aroncgg <aron.cevallosg@ucuenca.edu.ec>
2024-09-23 10:19:39 -0500

    00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/arcgg.py
@@ -0,0 +1,45 @@
+'''
+EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ '''
+
+# https://www.python.org/
+
+# Una linea 
+
+''' Para 
+varias
+ lineas '''
+
+""" Para
+ varias lineas
+   """
+
+' Una linea '
+
+
+miVariable = "variable"
+miVariable = 'nuevaVariable'
+
+
+MY_CONSTANT = 'mi Constante' #Al estar en mayúsculas, representa que esta constante no debe de cambiar
+
+
+int = 1 # variable INT de números enteros, aquellos que no tienen decimales
+float = 1.25 # variable que acepta números decimales, porque es una variable flotante
+bool = True #Booleano, es una variable que sigue una lógica de V o F
+bool = False # Verdadero=True, Falso=False
+string = "cadena de texto"
+string2 = 'cadena de texto con otro tipo de comillas '
+
+
+print("Imprimo un string o cadena de texto")
+print('Hola, Python!')
